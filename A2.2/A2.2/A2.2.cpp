6a0c72b10656b02887274577c8b153aabf3e9c21
andybloke <andeee@riseup.net>
2016-05-17 21:05:06 +0200

    Aufgabe angefangen

---
+++ A2.2/A2.2/A2.2.cpp
@@ -0,0 +1,28 @@
+#include "feld.h"
+// Einstiegspunkt des Programms
+
+
+int main()
+{
+	// Die Position unseres Markers auf dem Feld, zum Start
+	// setzen wir diese auf 0,0 welches die Mitte des Feldes sein soll
+	int markerX = 0;
+	int markerY = 0;
+	// Solange der Marker im Feld ist soll die Schleife laufen
+	while (true)
+	{
+		// Diese Anweisung löscht den Inhalt der Konsole
+		system("cls");
+		// Gib das Feld aus und die Markierung
+		printPositionInField(markerX, markerY);
+		// Prüfe ob die Markierung mit dem Rand kollidiert,
+		// wenn ja brechen wir die Schleife ab
+		if (checkCollision(markerX, markerY))
+			break;
+		// Verschiebe den Marker in eine beliebige Richtung
+		markerX += rand() % 5 - 2;
+		markerY += rand() % 5 - 2;
+	}
+	system("pause");
+	return 0;
+}
