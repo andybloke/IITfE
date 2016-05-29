9d91890202c71a13ec62e7b1e8e2ebcf72f8555b
AndyBloke <andeee@riseup.net>
2016-05-29 11:34:57 +0200

    getInput, checkForCopy & testInputRange added

--- HA3.3/HA3.3/Source.cpp
+++ HA3.3/HA3.3/Source.cpp
@@ -6,7 +6,32 @@ using namespace std;
 
 bool testInputRange(int input)
 {
+	if (input < 1 || input > 49)
+		return false;
+	else
+		return true;
+}
 
+bool checkForCopy(vector<int> userNumbers)
+{
+	for (int i = 0; i < userNumbers.size(); ++i)
+	{
+		int control = userNumbers[i];
+		int count = 0;
+		for (int x = 0; x < userNumbers.size(); ++x)
+		{
+			if (control == userNumbers[x])
+				count++;
+			else
+				continue;
+		}
+		if (count > 1)
+			return true;
+		else
+			continue;
+	}
+
+	return false;
 }
 
 vector<int> getInput()
@@ -16,11 +41,16 @@ vector<int> getInput()
 	int input;
 	for (int i = 0; i < MAX_NUM; ++i)
 	{
-		do
+		cout << "Bitte geben Sie Ihre " << i + 1 << "te Zahl ein." << '\n' << "-->";
+		cin >> input;
+		cout << '\n';
+		while (testInputRange(input) == false)
 		{
-			cout << "Bitte geben Sie Ihre " << i + 1 << "te Zahl ein." << '\n' << "-->";
+			cout << "Bitte geben Sie eine Zahl zwichen 1 und 49 ein" << '\n' << "-->";
+			cin.clear();
 			cin >> input;
-		} while (testInputRange(input) = false);
+			cout << '\n';
+		} 
 				
 		userNumbers.push_back(input);
 	}
@@ -30,4 +60,10 @@ vector<int> getInput()
 int main()
 {
 	vector<int> userNumbers = getInput();
+	if (checkForCopy(userNumbers) == true)
+	{
+		cout << "FEHLER: Jeder Zahl darf nur ein Mal eingegeben sein" << '\n';
+		system("PAUSE");
+		return -1;
+	}
 }
